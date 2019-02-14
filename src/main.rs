1933c7e9a887f337568f14e09b427eb91bc62e93
jimtahu <jimtahu@gmail.com>
2019-02-14 04:11:15 +0000

    Add Tile and Entity

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,79 @@ use quicksilver::{
     Future, Result
 };
 
+#[derive(Clone, Debug, PartialEq)]
+struct Tile {
+    pos: Vector,
+    glyph: char,
+    color: Color,
+}
+
+#[derive(Clone, Debug, PartialEq)]
+struct Entity {
+    pos: Vector,
+    glyph: char,
+    color: Color,
+    hp: i32,
+    max_hp: i32,
+}
+
+fn generate_entities( ) -> Vec<Entity> {
+    vec![
+        Entity {
+            pos: Vector::new(9, 6),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(2, 4),
+            glyph: 'g',
+            color: Color::RED,
+            hp: 1,
+            max_hp: 1,
+        },
+        Entity {
+            pos: Vector::new(7, 5),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+        Entity {
+            pos: Vector::new(4, 8),
+            glyph: '%',
+            color: Color::PURPLE,
+            hp: 0,
+            max_hp: 0,
+        },
+    ]
+}
+
+fn generate_map( size: Vector ) -> Vec<Tile> {
+    let width = size.x as usize;
+    let height = size.y as usize;
+    let mut map = Vec::with_capacity( width * height );
+    for x in 0..width {
+        for y in 0..height {
+            let mut tile = Tile {
+                pos: Vector::new( x as f32, y as f32 ),
+                glyph: '.',
+                color: Color::BLACK,
+            };
+            map.push(tile);
+        }
+    }
+    map
+}
+
 struct Game {
     title: Asset<Image>,
     square_font_info: Asset<Image>,
+    map_size: Vector,
+    map: Vec<Tile>,
+    entities: Vec<Entity>,
+    player_id: usize,
 }
 
 impl State for Game {
@@ -20,7 +90,29 @@ impl State for Game {
        let square_font_info = Asset::new(Font::load(font_square).and_then(|font| {
            font.render("Square font by Wouter Van Oortmerssen, terms: CC BY 3.0", &FontStyle::new(12.0, Color::BLACK))
        }));
-       Ok(Self{ title, square_font_info, })
+       let game_glyphs = "#@g.%";
+
+       let map_size = Vector::new( 40, 40 );
+       let map = generate_map( map_size );
+       let mut entities = generate_entities();
+       let player_id = entities.len();
+
+       entities.push(Entity {
+           pos: Vector::new(5, 3),
+           glyph: '@',
+           color: Color::BLUE,
+           hp: 3,
+           max_hp: 5,
+       });
+
+       Ok(Self{
+           title,
+           square_font_info,
+           map_size,
+           map,
+           entities,
+           player_id,
+       })
    }
 
    /// Process input / update game state
