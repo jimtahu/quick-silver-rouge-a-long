ebe57791a10f1226432c24073db0cbf07260511b
jimtahu <jimtahu@gmail.com>
2021-02-24 21:08:01 -0600

    Add key inputs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::input::Key;
+use tcod::input::KeyCode::*;
 
 // window size
 const SCREEN_WIDTH: i32 = 80;
@@ -8,6 +10,26 @@ const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
     root: Root,
+}
+
+fn handle_keys( tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32 ) -> bool
+{
+    let key = tcod.root.wait_for_keypress(true);
+    match key {
+        Key { code: Escape, .. } => return true,
+        Key { code: Enter, alt: true, .. } => {
+            let fullscreen = tcod.root.is_fullscreen();
+            tcod.root.set_fullscreen(!fullscreen);
+        }
+
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        _ => {}
+    }
+    false
 }
 
 fn main() {
@@ -20,11 +42,14 @@ fn main() {
     let mut tcod = Tcod { root };
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
     while !tcod.root.window_closed() {
         tcod.root.set_default_foreground(WHITE);
         tcod.root.clear();
-        tcod.root.put_char( 1, 1, '@', BackgroundFlag::None );
+        tcod.root.put_char( player_x, player_y, '@', BackgroundFlag::None );
         tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
+        let exit = handle_keys( &mut tcod, &mut player_x, &mut player_y);
+        if exit { break; }
     }
 }
