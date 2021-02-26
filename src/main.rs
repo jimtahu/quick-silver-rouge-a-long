d6dab1c3059f44e0390ac6d18f574fdfe5945cba
jimtahu <jimtahu@gmail.com>
2021-02-25 21:09:45 -0600

    Add create_room

--- src/main.rs
+++ src/main.rs
@@ -65,15 +65,44 @@ impl Tile {
 
 type Map = Vec<Vec<Tile>>;
 
+#[derive(Clone, Copy, Debug)]
+struct Rect {
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+impl Rect {
+    pub fn new( x: i32, y: i32, w: i32, h: i32 ) -> Self {
+        Rect {
+            x1: x,
+            y1: y,
+            x2: x+w,
+            y2: y+h,
+        }
+    }
+}
+
+fn create_room( room: Rect, map: &mut Map ) {
+    for x in (room.x1+1)..room.x2 {
+        for y in (room.y1+1)..room.y2 {
+            map[x as usize][y as usize] = Tile::empty();
+        }
+    }
+}
+
 struct Game {
     map: Map,
 }
 
 fn make_map() -> Map {
     // fill with empty tiles
-    let mut map = vec![vec![Tile::empty(); MAP_HEIGHT as usize]; MAP_WIDTH as usize ];
-    map[30][22] = Tile::wall();
-    map[50][22] = Tile::wall();
+    let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize ];
+    let room1 = Rect::new(20,15,10,15);
+    let room2 = Rect::new(50,15,10,15);
+    create_room(room1, &mut map);
+    create_room(room2, &mut map);
     map
 }
 
@@ -144,7 +173,7 @@ fn main() {
         map: make_map(),
     };
 
-    let player = Object::new( SCREEN_WIDTH/2, SCREEN_HEIGHT/2, '@', WHITE );
+    let player = Object::new( 25, 23, '@', WHITE );
     let npc = Object::new( SCREEN_WIDTH/2 - 5, SCREEN_HEIGHT/2, '@', YELLOW );
     let mut objects = [ player, npc ];
     while !tcod.root.window_closed() {
